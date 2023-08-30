d1b6dd54ec011deeb266de07170c6f459bed7358
LucasDCoelho <darkalp99@gmail.com>
2023-08-30 16:33:37 -0300

    finished: the History Component with styles

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './style'
+import { HistoryContainer, HistoryList, Status } from './style'
 
 export function History() {
   return (
@@ -20,37 +20,49 @@ export function History() {
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há cerca de 1 hora</td>
-              <td>Concluida</td>
+              <td>
+                <Status statusColor="green">Concluida</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há cerca de 1 hora</td>
-              <td>Concluida</td>
+              <td>
+                <Status statusColor="green">Concluida</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há cerca de 1 hora</td>
-              <td>Concluida</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há cerca de 1 hora</td>
-              <td>Concluida</td>
+              <td>
+                <Status statusColor="red">Interrompida</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há cerca de 1 hora</td>
-              <td>Concluida</td>
+              <td>
+                <Status statusColor="green">Concluida</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há cerca de 1 hora</td>
-              <td>Concluida</td>
+              <td>
+                <Status statusColor="green">Concluida</Status>
+              </td>
             </tr>
           </tbody>
         </table>
